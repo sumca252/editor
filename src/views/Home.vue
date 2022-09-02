a9e3f124f770c095eef950d63f70b84dd437c231
sumca252 <70111426+sumca252@users.noreply.github.com>
2022-09-02 11:13:06 +0200

    makes a better structure

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="m-3">
+        <EditorItem />
+    </div>
+</template>
+
+<script>
+import EditorItem from "@/components/Editor/Editor.vue";
+export default {
+    name: "HomeItem",
+    components: {
+        EditorItem,
+    },
+};
+</script>
+
+<style>
+.ql-editor {
+    height: 600px;
+}
+</style>
