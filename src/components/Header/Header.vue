33bc539deaf6e57ec90d224aab97f2fa3d34e7d8
sumca252 <ismail.ali9315@gmail.com>
2022-09-08 20:40:46 +0200

    removes save button from header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -31,13 +31,6 @@
                         >Editor</span
                     >
                 </router-link>
-                <div class="flex items-center lg:order-2">
-                    <ButtonItem
-                        text="Save"
-                        icon="fa-solid fa-floppy-disk mr-2"
-                        @click="handleBtnSaveClick"
-                    />
-                </div>
             </div>
         </nav>
     </header>
