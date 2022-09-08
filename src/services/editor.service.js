42b4935be185c9204ba4c2025d82fc076df77aa1
sumca252 <ismail.ali9315@gmail.com>
2022-09-08 21:01:53 +0200

    adds update data service

--- src/services/editor.service.js
+++ src/services/editor.service.js
@@ -12,6 +12,10 @@ class EditorService {
     getDataById(id) {
         return axios.get(`/editor/${id}`);
     }
+
+    updateData(id, data) {
+        return axios.put(`/editor/${id}`, data);
+    }
 }
 
 export default new EditorService();
