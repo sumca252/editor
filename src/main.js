b6e7d37761671c8a4b8b1a7f83cdc8cc9187cf5c
sumca252 <ismail.ali9315@gmail.com>
2022-09-08 20:42:37 +0200

    sets base url and headers

--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import "@/style.css";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 createApp(App).use(store).use(router).mount("#app");
