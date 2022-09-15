506176366a1ef1095dcfffd8171fe6f9217a7411
sumca252 <ismail.ali9315@gmail.com>
2022-09-15 11:45:22 +0200

    based on url origin use local or azure

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import router from "@/router";
 import store from "@/store";
 import axios from "axios";
 
-axios.defaults.baseURL =
-    "https://jsramverk-editor-isal-20.azurewebsites.net/api";
+axios.defaults.baseURL = window.location.origin.includes("localhost")
+    ? "http://localhost:3000/api"
+    : "https://jsramverk-editor-isal-20.azurewebsites.net/api";
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
