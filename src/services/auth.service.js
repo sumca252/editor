541098522f99bc0030732a8b6b7124c92714e779
sumca252 <70111426+sumca252@users.noreply.github.com>
2022-09-28 16:04:49 +0200

    adds authentication service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,6 +4,10 @@ class AuthService {
     registerUser(user) {
         return axios.post("/auth/register", user);
     }
+
+    loginUser(user) {
+        return axios.post("/auth/login", user);
+    }
 }
 
 export default new AuthService();
