fd0d039a032a77cfe9c7d964efbf83b41fac12b7
sumca252 <70111426+sumca252@users.noreply.github.com>
2022-09-28 16:03:29 +0200

    update updates page
    adds shared with field to show which users the document is shared with.

--- src/views/UpdateView.vue
+++ src/views/UpdateView.vue
@@ -15,6 +15,27 @@
         <div class="mx-auto max-w-screen-xl p-4">
             <form class="w-full pb-10" @submit.prevent="updateText">
                 <div class="flex items-center border-b border-gray-500 py-2">
+                    <label class="text-gray-600 font-bold">Shared</label>
+                    <input
+                        class="
+                            appearance-none
+                            bg-transparent
+                            border-none
+                            w-full
+                            text-gray-700
+                            mr-3
+                            py-1
+                            px-2
+                            leading-tight
+                            focus:outline-none
+                        "
+                        type="email"
+                        name="email"
+                        v-model="email"
+                        :disabled="!docRef"
+                    />
+                </div>
+                <div class="flex items-center border-b border-gray-500 py-2">
                     <input
                         class="
                             appearance-none
@@ -67,23 +88,50 @@
     </div>
 </template>
 <script setup>
+/**
+ * imports
+ */
 import { ref, onMounted, watch, watchEffect, onUnmounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { io } from "socket.io-client";
 import EditorService from "../services/editor.service";
+import { useUserStore } from "../store/user";
 
+/**
+ * socket
+ */
+const socket = io("http://localhost:3000");
+
+/**
+ * refs
+ */
 const editorRef = ref({});
+const email = ref("");
 const title = ref("");
 const content = ref("");
+const docRef = ref(false);
 
+/**
+ * store
+ */
+
+const userStore = useUserStore();
+
+/**
+ * router
+ */
 const route = useRoute();
 const router = useRouter();
 
+/**
+ * methods
+ */
 const updateText = async () => {
     try {
         let updatedData = {
             title: title.value,
             content: JSON.stringify(content.value),
+            email: email.value,
         };
 
         let response = await EditorService.updateData(
@@ -95,7 +143,7 @@ const updateText = async () => {
             router.push({ name: "Home" });
         }
     } catch (error) {
-        console.log(error);
+        throw new Error(error);
     }
 };
 
@@ -103,13 +151,25 @@ const getDataById = async () => {
     try {
         let response = await EditorService.getDataById(route.params.id);
 
+        email.value = response.data.data.allowed_users;
         title.value = response.data.data.title;
-
         editorRef.value.setContents(JSON.parse(response.data.data.content));
+
+        if (response.data.data.author === userStore.user.email) {
+            docRef.value = true;
+        }
     } catch (error) {
         console.log(error);
     }
 };
+
+const onTextChange = (delta) => {
+    socket.emit("send-changes", delta, route.params.id);
+};
+
+/**
+ * lifecycle Hooks
+ */
 
 onMounted(() => {
     getDataById();
@@ -120,15 +180,9 @@ onMounted(() => {
     });
 });
 
-const socket = io("http://localhost:3000");
-
 onUnmounted(() => {
     socket.disconnect();
 });
-
-const onTextChange = (delta) => {
-    socket.emit("send-changes", delta, route.params.id);
-};
 </script>
 
 <style>
