4e6cfed25d2dae6a7cf828c2160a19796c89b791
sumca252 <70111426+sumca252@users.noreply.github.com>
2022-09-28 16:01:22 +0200

    users store

---
+++ src/store/user.js
@@ -0,0 +1,53 @@
+import { defineStore } from "pinia";
+import AuthService from "@/services/auth.service";
+
+const user = JSON.parse(localStorage.getItem("user")) || [];
+const initialState = user
+    ? { status: { loggedIn: true }, user }
+    : { status: { loggedIn: false }, user: null };
+
+export const useUserStore = defineStore({
+    id: "user",
+    state: () => initialState,
+    getters: {
+        isLoggedIn: (state) => state.status.loggedIn,
+        getUser: (state) => state.user,
+        getUserEmail: (state) => state.user.email,
+    },
+    actions: {
+        async register(user) {
+            try {
+                const response = await AuthService.registerUser(user);
+                if (response.data.user) {
+                    localStorage.setItem("email", response.data.user.email);
+                }
+                return response.data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+
+        async login(user) {
+            try {
+                const response = await AuthService.loginUser(user);
+                if (response.data.user) {
+                    localStorage.setItem(
+                        "user",
+                        JSON.stringify(response.data.user)
+                    );
+                    this.user = response.data.user;
+                    this.status.loggedIn = true;
+                }
+                return response.data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+
+        logout() {
+            this.status.loggedIn = false;
+            this.user = null;
+            localStorage.removeItem("user");
+        },
+    },
+});
