e08d9fbcca389db748d78da79d1a48f669e43964
sumca252 <ismail.ali9315@gmail.com>
2022-10-20 16:19:02 +0200

    adds id for testing purpose

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -36,7 +36,7 @@
                         <RouterLink :to="{ name: 'Home'}" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" active-class="active">Home</RouterLink>
                         </li>
                         <li>
-                        <RouterLink :to="{ name: 'CodeMode'}" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" active-class="active">Codemode</RouterLink>
+                        <RouterLink :to="{ name: 'CodeMode'}" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" active-class="active" id="codemode">Codemode</RouterLink>
                         </li>
                     </ul>
                 </div>
